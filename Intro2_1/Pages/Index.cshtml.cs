536740f925849250b54c546145cba9b93c6ac657
Aoniq <89135@glr.nl>
2023-09-10 00:37:03 +0200

    Intro2 file uploader

--- Intro2_1/Pages/Index.cshtml.cs
+++ Intro2_1/Pages/Index.cshtml.cs
@@ -5,16 +5,68 @@ namespace Intro2_1.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    [BindProperty]
+    public IFormFile File { get; set; }
 
-    public IndexModel(ILogger<IndexModel> logger)
+
+
+    public string ResultMessage { get; set; }
+
+
+
+    public async Task<IActionResult> OnPostAsync()
     {
-        _logger = logger;
-    }
+        if (File != null && File.Length > 0)
+        {
+            try
+            {
+                // Stap 1: Controleer of het bestand een JPEG-bestand is
+                string fileExtension = Path.GetExtension(File.FileName);
+                if (fileExtension != ".jpeg" && fileExtension != ".jpg")
+                {
+                    ResultMessage = "Alleen JPEG-bestanden (.jpeg of .jpg) zijn toegestaan.";
+                }
+                else
+                {
+                    // Bepaal het pad naar de uploadsmap
+                    string uploadsPath = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
 
-    public void OnGet()
-    {
+                    // Zorg ervoor dat de uploadsmap bestaat, zo niet, maak deze aan
+                    if (!Directory.Exists(uploadsPath))
+                    {
+                        Directory.CreateDirectory(uploadsPath);
+                    }
 
+                    // Genereer een unieke bestandsnaam om conflicten te voorkomen
+                    string uniqueFileName = Guid.NewGuid().ToString() + fileExtension;
+
+                    // Volledig pad naar het opgeslagen bestand
+                    string filePath = Path.Combine(uploadsPath, uniqueFileName);
+
+                    // Verplaats het geüploade bestand naar de uploadsmap
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await File.CopyToAsync(stream);
+                    }
+
+                    ResultMessage = "Bestand is met succes geüpload en opgeslagen in de uploadsmap.";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultMessage = "Er is een fout opgetreden tijdens het verwerken van het bestand: " + ex.Message;
+            }
+        }
+        else
+        {
+            ResultMessage = "Selecteer een bestand om te uploaden, anders gaat het niet werken!";
+        }
+
+
+
+
+        return Page();
     }
 }
 
